b037030aabbd20340ddde557e7755aeff368a86e
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2026-01-25 20:09:24 +0100

    Update RT to 271.2.0 (#42646)

--- Content.Server/Entry/EntryPoint.cs
+++ Content.Server/Entry/EntryPoint.cs
@@ -34,6 +34,7 @@ using Robust.Server.ServerStatus;
 using Robust.Shared.Configuration;
 using Robust.Shared.ContentPack;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
 using Content.Server._Adventure; // AdvSpace Config
@@ -96,6 +97,8 @@ namespace Content.Server.Entry
                 var cast = (ServerModuleTestingCallbacks)callback;
                 cast.ServerBeforeIoC?.Invoke();
             }
+
+            Dependencies.Resolve<IRobustSerializer>().FloatFlags = SerializerFloatFlags.RemoveReadNan;
         }
 
         /// <inheritdoc />
